ee6c7513818638c38e83093d4c9d11c743ce89ae
Mario Dorado Martínez <44005739+mdoradom@users.noreply.github.com>
2023-02-27 12:14:38 +0100

    Add project files.

---
+++ Project1/2.asm
@@ -0,0 +1,34 @@
+.686
+.model flat
+
+.stack 1000h
+
+; C CODE
+; int i = 42
+; int *pi
+; int j
+; pi = &i
+; j = i
+; *pi = -1
+
+; IN ASSEMBLY
+
+
+.data
+i DD 42
+pi DD ?
+j DD ?
+
+.code
+_main proc
+MOV EAX, [i]
+MOV [pi], EAX ; *
+
+MOV EBX, i
+MOV j, EBX
+
+MOV [EAX], -1 ; *
+
+
+_main endp
+end _main
