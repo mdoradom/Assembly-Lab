c7c213ea15c1642e05eb8770b1fc1e70741a3a49
Mario Dorado Martínez <44005739+mdoradom@users.noreply.github.com>
2023-03-13 11:54:55 +0100

    cositas 🔥🔥👌👌

---
+++ Project1/task2.asm
@@ -0,0 +1,30 @@
+.686
+.model flat
+
+.stack 1000h
+
+.data
+    dividend    dd  20  ; the dividend
+    divisor     dd  5   ; the divisor
+    quotient    dd  0   ; the quotient
+    remainder   dd  0   ; the remainder
+
+.code
+_main proc
+    mov eax, [dividend]      ; load dividend into eax
+    mov ebx, [divisor]       ; load divisor into ebx
+    mov ecx, [quotient]      ; load quotient into ecx
+    mov edx, [remainder]     ; load remainder into edx
+
+div_loop:
+    cmp eax, ebx             ; compare dividend and divisor
+    jl div_done              ; if dividend < divisor, division is done
+    
+    sub eax, ebx             ; subtract divisor from dividend
+    add ecx, 1               ; increment quotient
+    jmp div_loop             ; repeat division
+div_done:
+
+_main endp
+
+end _main
